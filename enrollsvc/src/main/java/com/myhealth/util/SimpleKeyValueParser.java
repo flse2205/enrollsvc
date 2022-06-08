24f950b903b557d0f727d47d0f9aa0cc66130ad4
hshaik <flse20209@gmail.com>
2022-06-08 16:59:43 -0700

    SimpleKeyValueParser commit

---
+++ enrollsvc/src/main/java/com/myhealth/util/SimpleKeyValueParser.java
@@ -0,0 +1,26 @@
+package com.myhealth.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SimpleKeyValueParser {
+
+	Map<String, String> parseRequest(String requestStr) {
+		Map<String, String> map = new HashMap<String, String>();
+		String[] arrayStr = requestStr.split(",");
+		for (String str : arrayStr) {
+			String[] kvStr = str.split(":");
+			map.put(kvStr[0], kvStr[1]);
+		}
+		return map;
+	}
+
+	boolean validateEntry(String key, String value) {
+		if (key.startsWith("\"") && key.endsWith("\"") 
+				&& value.startsWith("\"") && value.endsWith("\"")) {
+			return true;
+		}
+		return false;
+
+	}
+}
